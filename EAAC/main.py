e8cd5c0a91e20370e4424dd758fabc16631ded96
idan shenfeld <idanshen@mit.edu>
2022-11-22 15:39:29 -0500

    fix imports

--- EAAC/main.py
+++ EAAC/main.py
@@ -1,3 +1,15 @@
+
+
+from inspect import getsourcefile
+import os.path
+import sys
+
+current_path = os.path.abspath(getsourcefile(lambda:0))
+current_dir = os.path.dirname(current_path)
+parent_dir = current_dir[:current_dir.rfind(os.path.sep)]
+
+sys.path.insert(0, parent_dir)
+
 from EAAC.wrappers import make
 from omegaconf import DictConfig
 from stable_baselines3 import SAC, EAAC
